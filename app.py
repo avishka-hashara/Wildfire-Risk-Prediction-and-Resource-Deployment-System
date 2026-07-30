8bb9e42619d0f8eb92704501863142a641abfa0e
Avishka <eaahashara01@gmail.com>
2026-07-30 12:30:39 +0530

    chore: disable numba JIT via environment variable in app startup

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+os.environ['NUMBA_DISABLE_JIT'] = '1'
+
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
